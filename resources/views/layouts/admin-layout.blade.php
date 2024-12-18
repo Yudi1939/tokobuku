24aad5873d9f7033b128be9dc8ad0c99c9a5c076
RikiArtha77 <kadekriki2515@gmail.com>
2024-12-18 14:29:35 +0800

    tambahan page

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -43,7 +43,7 @@ $users = [
             </div>
     
             <nav class="mt-10">
-                <a class="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25" href="#">
+                <a class="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25" href="{{ route('daftarbuku') }}">
                     <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -52,11 +52,11 @@ $users = [
                             d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
                     </svg>
     
-                    <span class="mx-3">Dashboard</span>
+                    <span class="mx-3">Daftar Buku</span>
                 </a>
     
                 <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
-                    href="#">
+                    href="{{ route('daftaruser') }}">
                     <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -64,7 +64,7 @@ $users = [
                         </path>
                     </svg>
     
-                    <span class="mx-3">UI Elements</span>
+                    <span class="mx-3">Daftar user</span>
                 </a>
             </nav>
         </div>
